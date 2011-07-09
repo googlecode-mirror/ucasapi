046d04837cbbd1abeba8c81220a014b89e75452d
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-07-09 20:24:13 +0000

    Versión "mejorada del ejemplo"

--- trunk/application/controllers/departamento.php
+++ trunk/application/controllers/departamento.php
@@ -3,58 +3,54 @@ class Departamento extends CI_Controller{
 
 	function index(){
 		$this->load->helper(array('form', 'url'));
-		$this->load->view("departamento_view");
+		$this->load->view("departamentoView");
 	}
 	
 	function departmentRead(){
-		$this->load->model("departamento_model");	
-		echo json_encode($this->departamento_model->read());
+		$this->load->model("departamentoModel");	
+		echo json_encode($this->departamentoModel->read());
 	}
 	
 	
-	//Retorna en formato json los datos necesarios para mostrar en el autocomplete
 	function departmentAutocompleteRead(){
-		$this->load->model("departamento_model");	
-		echo json_encode($this->departamento_model->autocompleteRead());
+		$this->load->model("departamentoModel");
+		
+		$autocompleteData = $this->departamentoModel->autocompleteRead();		
+		
+		echo json_encode($autocompleteData);
+	}
+	
+	function departmentDelete(){
+		$this->load->model("departamentoModel");
+		
+		$deleteInfo = $this->departamentoModel->delete();		
+		
+		echo json_encode($deleteInfo);
 	}
 
 
-	/*Asumiendo en esta parte que la variable $status servirá para determinar el tipo de mensaje que se mostrará:
-		 0: Mensaje de error
-		 1: Mensaje de éxito
-		 2: Mensaje de información */	
 	function departmentValidateAndSave(){
-		$this->load->model("departamento_model");
-		$returnMsg = $this->departamento_model->saveValidation();
-		$status = 1;
+		$this->load->model("departamentoModel");
+		$retArray = array();
 		
-		if($returnMsg == ""){//Los datos ingresados no pasaron las validaciones
+		$validationInfo = $this->departamentoModel->saveValidation();
+		
+		if($validationInfo["status"] == 0){//Los datos ingresados pasaron las validaciones
 			$idDepto =  $this->input->post("idDepto");
 			
 			if($idDepto == ""){//Si no se recibe el id, los datos se guardarán como un nuevo registro
-				if($this->departamento_model->create()){//create retorna el número de filas afectadas
-					$status = 1;
-				}
-				else{
-					$status = 0;
-				}
+				$retArray = $this->departamentoModel->create();
 			}
 			else{
-				if($this->departamento_model->update()){
-					$status = 1;
-				}
-				else{
-					$status = 0;
-				}
+				$retArray = $this->departamentoModel->update();
 			}
-			
+						
 		}		
 		else{//Los datos ingresados no pasaron las validaciones
-			$status = 2;
+			$retArray = $validationInfo;
 		}
 		
-		$returnDataArray = array("status" => $status,"msg" => $returnMsg);
-		echo json_encode($returnDataArray);	
+		echo json_encode($retArray);	
 	}
 	
 }
