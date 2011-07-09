721c413c3284f79802e54979b3adb9a17cb84b4c
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-07-09 20:24:52 +0000

    Versión "mejorada del ejemplo"

--- application/views/js/departamento.js
+++ application/views/js/departamento.js
@@ -3,35 +3,47 @@ $(document).ready(function(){
 	departmentAutocomplete();		
 });	
 
-
-
-
 function departmentAutocomplete(){
 	$.ajax({				
         type: "POST",
         url:  "index.php/departamento/departmentAutocompleteRead",
-        data: "",
+        data: "departmentAutocomplete",
         dataType : "json",
-        success: function(dat){
-        	$( "#txtRecords" ).autocomplete({
-        		minChars: 0,  
-		        source: dat,
-		        minLength: 1,
-		        select: function( event, ui) {
-			        $("#idDepto").val(ui.item.id);					
-				}
-			});
+        success: function(retrievedData){        	
+        	if(retrievedData.status != 0){
+        		alert("Mensaje de error: " + retrievedData.msg); //Por el momento, el mensaje que se está mostrando es técnico, para cuestiones de depuración
+        	}
+        	else{        		
+        		$("#txtRecords").autocomplete({
+            		minChars: 0,  
+    		        source: retrievedData.data,
+    		        minLength: 1,
+    		        select: function(event, ui) {
+    			        $("#idDepto").val(ui.item.id);					
+    				}
+    			});
+        		
+        	}        	
       }
       
-	});			
+	});		
 }
-
 
 function save(){			
 	var formData= "";
 	formData += "idDepto=" + $("#idDepto").val();
 	formData += "&nombreDepto=" + $("#txtDepartmentName").val();
-	formData += "&descripcion=" + $("#txtDepartmentDesc").val();		
+	formData += "&descripcion=" + $("#txtDepartmentDesc").val();
+	
+	/*var Departamento = {
+			"idDepto" : $("#idDepto").val(),
+			"nombreDepto" : $("#txtDepartmentName").val(),
+			"descripcion" : $("#txtDepartmentDesc").val()			
+	}
+	
+	departamentoStr = JSON.stringify(Departamento);
+	
+	alert(departamentoStr);*/
 	
 	$.ajax({				
         type: "POST",
@@ -39,14 +51,19 @@ function save(){
         data: formData,
         dataType : "json",
         success: function(retrievedData){
-			        switch(retrievedData.status){
-			        	case 0 : alert("Mensaje de error: "+ retrievedData.msg);
-			        			 break;
-			        	case 1 : alert("Mensaje de éxito: "+ retrievedData.msg);
-	        			 		 break;
-	        			case 2 : alert("Mensaje de información: "+ retrievedData.msg);
-   			 		 			 break;			        
-			        }
+        	if(retrievedData.status != 0){
+        		alert("Mensaje de error: " + retrievedData.msg); //Por el momento, el mensaje que se está mostrando es técnico, para cuestiones de depuración
+        	}
+        	else{
+        		if($("idDepto").val()==""){
+        			alert("Registro agregado con éxito");
+        		}
+        		else{
+        			alert("Registro actualizado con éxito");
+        		}
+        		departmentAutocomplete();
+        		clear();
+        	}
       	}
       
 	});
@@ -62,30 +79,41 @@ function edit(){
         data: formData,
         dataType : "json",
         success: function(retrievedData){
-			       $("#txtDepartmentName").val(retrievedData.nombreDepto);
-			       $("#txtDepartmentDesc").val(retrievedData.descripcion);
-      	}
-      
+        	if(retrievedData.status != 0){
+        		alert("Mensaje de error: " + retrievedData.msg); //Por el momento, el mensaje que se está mostrando es técnico, para cuestiones de depuración
+        	}else{
+        		$("#txtDepartmentName").val(retrievedData.data.nombreDepto);
+			    $("#txtDepartmentDesc").val(retrievedData.data.descripcion);
+        	}			       
+      	}      
 	});
 	
 }
 
-function deleteData(){		
-	alert("Pendiente....");	
-	/*var formData = "idDepto=" + $("#idDepto").val();	
+function deleteData(){
+	var formData = "idDepto=" + $("#idDepto").val();
 	
-	$.ajax({				
-        type: "POST",
-        url:  "index.php/departamento/departmentDelete",
-        data: formData,
-        dataType : "json",
-        success: function(retrievedData){
-			       $("#txtDepartmentName").val(retrievedData.nombreDepto);
-			       $("#txtDepartmentDesc").val(retrievedData.descripcion);
-      	}
-      
-	});*/
+	var answer = confirm("Está seguro que quiere eliminar el registro: "+ $("#txtRecords").val()+ " ?");
 	
+	if (answer){		
+		$.ajax({				
+	        type: "POST",
+	        url:  "index.php/departamento/departmentDelete",
+	        data: formData,
+	        dataType : "json",
+	        success: function(retrievedData){
+	        	if(retrievedData.status != 0){
+	        		alert("Mensaje de error: " + retrievedData.msg); //Por el momento, el mensaje que se está mostrando es técnico, para cuestiones de depuración
+	        	}
+	        	else{
+	        		alert("Registro eliminado con éxito");
+	        		departmentAutocomplete();
+	        		clear();
+	        	}
+	      	}
+	      
+		});		
+	}	
 }
 
 function cancel(){
