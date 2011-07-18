616d3f2b16d6b7ece2e51ef1c6877ef3e0234ccb
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-07-18 01:43:23 +0000

    Modificados algunos elementos para implementación del style.css

--- application/views/departamentoView.php
+++ application/views/departamentoView.php
@@ -1,7 +1,8 @@
 <html>
 	<head>
 		<title>Test</title>		
-		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/humanity/jquery-ui-1.8.14.custom.css" rel="stylesheet" />	
+		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/humanity/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
+		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/style.css" rel="stylesheet" />	
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/jquery-1.5.1.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/jquery-ui-1.8.14.custom.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/departamento.js"></script>
@@ -9,21 +10,32 @@
 	</head>
 	
 	<body>
-	
-		<div id="divActions" class="ActionPane">	
-			
-			<div id="divCRUDRecords">
-				<span class = "recordsLabel">Departamentos</span>
-				<input id="txtRecords" type="text"  value=""/><br>
-			</div>			
-			<div id="divCRUDButtons">
-				<input id = "btnSave" type="button" value="Guardar" onClick="save()"/><br>
-				<input id = "btnEdit" type="button" value="Editar" onClick="edit()"/><br>
-				<input id = "btnDelete" type="button" value="Eliminar" onClick="deleteData()"/><br>
-				<input id = "btnCancel" type="button" value="Cancelar" onClick="cancel()"/><br>
+		<div class="menu_bar">
+			<ul>
+				<li><span class="menu_button_to"><a href="http://www.googlecom"><span class="menu_button_text">Dinamic</span></a></span></li>
+				<li class="highlight"><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
+				<li><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
+				<li><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
+			</ul>			
+		</div>
+		<div class="user_session">	
+		</div>
+		<div class="container">
+			<div style="height: 20px"></div>
+			<div class="divActions">				
+				<div id="divCRUDRecords">
+					<span class = "recordsLabel">Departamentos</span>
+					<input id="txtRecords" type="text"  value=""/><br>
+				</div>			
+				<div id="divCRUDButtons">
+					<input id = "btnSave" type="button" value="Guardar" onClick="save()"/>
+					<input id = "btnEdit" type="button" value="Editar" onClick="edit()"/>
+					<input id = "btnDelete" type="button" value="Eliminar" onClick="deleteData()"/>
+					<input id = "btnCancel" type="button" value="Cancelar" onClick="cancel()"/>
+				</div>
 			</div>
-			
-			<div id="divDataForm" class="DataForm" style>
+				
+			<div class="divDataForm">
 				<input id="idDepto" type="hidden"  value="" class = "hiddenId"/><br>
 			
 				<span class = "inputFieldLabel">Nombre</span>
@@ -32,7 +44,7 @@
 				<span class = "inputFieldLabel">Descripción</span>
 				<textArea id="txtDepartmentDesc" cols=20 rows=6 class = "inputField"></textArea>
 			</div>
-		
+			
 		</div>
 	
 		
