5e2e11b2324431a0b75482cfb361ee22cea7c049
nilsinho.1203 <nilsinho.1203@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-07-24 23:18:07 +0000

    Le puse que pusiera en la sesión el id del usuario

--- trunk/application/controllers/login.php
+++ trunk/application/controllers/login.php
@@ -17,7 +17,8 @@ class Login extends CI_Controller{
 		
 		if(count($userData["data"])>0){//Si se obtuvieron datos para las credenciales introducidas
 			$this->session->set_userdata("idRol", $userData["data"]["idRol"]);
-			$this->session->set_userdata("username", $userData["data"]["primerApellido"].$userData["data"]["primerNombre"]);			
+			$this->session->set_userdata("username", $userData["data"]["primerApellido"].$userData["data"]["primerNombre"]);
+			$this->session->set_userdata("idUsuario", $userData["data"]["idUsuario"]);
 			
 			$userData["msg"] = site_url("departamento");
 		}
