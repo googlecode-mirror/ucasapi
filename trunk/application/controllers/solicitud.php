f3da3a82a709859029b8dc61c0f58417cac9c1e5
nilsinho.1203 <nilsinho.1203@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-07-24 23:18:44 +0000

    Agregado el controlador para las solicitudes

---
+++ trunk/application/controllers/solicitud.php
@@ -0,0 +1,35 @@
+<?php
+
+class Solicitud extends CI_Controller {
+
+	function index() {
+		$this->load->library('session');
+		$this->load->helper(array('form', 'url'));		
+		
+		$idUsuario = $this->session->userdata("idUsuario");//Se agrega en $idRol el dato correspondiente de la sesi�n
+		
+		if($idUsuario == ""){//Si el dato no est� en sesi�n, se redirige a la p�gina de login
+			redirect("login", "refresh");
+		}
+		else{
+			$this->load->view("solicitudView");
+		}
+	}
+	
+	function solicitudSave() {
+		$this->load->model("solicitudModel");
+		$retArray = array();
+		
+		$validationInfo = $this->solicitudModel->saveValidation();
+		
+		if($validationInfo["status"] == 0){//Los datos ingresados pasaron las validaciones
+			$retArray = $this->solicitudModel->create();
+						
+		}		
+		else{//Los datos ingresados no pasaron las validaciones
+			$retArray = $validationInfo;
+		}
+		
+		echo json_encode($retArray);
+	}
+} 
