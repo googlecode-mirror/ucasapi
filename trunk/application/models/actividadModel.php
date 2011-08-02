f412458a19ca8db589c80ac1886a0c1eaffece63
jlozanoec <jlozanoec@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-02 23:27:26 +0000

    Actualizacion de actividad completada (solo falta la accion de desasignacion)

--- trunk/application/models/actividadModel.php
+++ trunk/application/models/actividadModel.php
@@ -2,6 +2,27 @@
 
 class actividadModel extends CI_Model{
 	
+	function readEstados(){
+		$this->load->database();
+		
+		$retArray = array();
+		
+		$sql = "SELECT idEstado, estado FROM Estado WHERE idTipoEstado = 1";
+		
+		$query = $this->db->query($sql);
+		
+		if($query->num_rows > 0){			
+			foreach ($query->result() as $row){		
+				$rowArray = array();
+				$rowArray["idEstado"] = $row->idEstado;
+				$rowArray["estado"] = $row->estado;
+				$retArray["data"][] = $rowArray;				
+			}							
+		}
+		
+		return $retArray;
+	}
+	
 	function read(){
 		$this->load->database();
 		
@@ -11,12 +32,14 @@ class actividadModel extends CI_Model{
 		$idProyecto = $this->input->post("idProyecto");
 		$idUsuario = $this->input->post("idUsuario");
 		
-		$sql = "SELECT p.nombreProyecto, a.nombreActividad, a.descripcionActividad, e.estado, b.progreso, b.comentario
-				 FROM Proyecto p INNER JOIN Actividad_Proyecto axp ON p.idProyecto = axp.idProyecto INNER JOIN Actividad a
-    			 ON axp.idActividad = a.idActividad INNER JOIN Estado e ON a.idEstado = e.idEstado INNER JOIN Bitacora b 
-    			 ON a.idActividad = b.idActividad
-				 WHERE b.correlativoBitacora = (SELECT MAX(correlativoBitacora) FROM Bitacora WHERE idActividad = ".$idActividad." AND idUsuario = ".$idUsuario.") 
-				 AND a.idActividad = " .$idActividad. " AND p.idProyecto = " .$idProyecto;
+		$sql = "SELECT CONCAT(u.primerNombre, ' ', u.primerApellido) nombreAsigna, p.nombreProyecto, a.nombreActividad, a.descripcionActividad, 
+					e.idEstado, e.estado, b.progreso, b.comentario
+				FROM Proyecto p INNER JOIN Actividad_Proyecto axp ON p.idProyecto = axp.idProyecto INNER JOIN Actividad a
+    				ON axp.idActividad = a.idActividad INNER JOIN Estado e ON a.idEstado = e.idEstado INNER JOIN Bitacora b 
+    			 	ON a.idActividad = b.idActividad INNER JOIN Usuario_Actividad uxa ON a.idActividad = uxa.idActividad
+    			 	INNER JOIN Usuario u ON uxa.idUsuarioAsigna = u.idUsuario
+				WHERE b.ultimoRegistro = (SELECT MAX(ultimoRegistro) FROM Bitacora WHERE idActividad = ".$idActividad.") 
+					AND a.idActividad = " .$idActividad. " AND p.idProyecto = " .$idProyecto." AND uxa.idUsuario = ".$idUsuario;
 				
 		$query = $this->db->query($sql);
 
@@ -44,18 +67,50 @@ class actividadModel extends CI_Model{
 		$idActividad = $this->input->post("idActividad");
 		$progreso = $this->input->post("progreso");
 		$comentario = $this->input->post("comentario");
+		$user_rows = $this->input->post("user_data");
 		
-		$sql = "UPDATE ACTIVIDAD SET idEstado = ".$idEstado." WHERE idActividad = ".$idActividad; 
+		
+		$this->db->trans_begin();
+		$sql = "UPDATE ACTIVIDAD SET idEstado = ".$idEstado." WHERE idActividad = ".$idActividad;
+		//echo "QUERY 1: " .$sql; 
 		$query = $this->db->query($sql);
 		$sql = "CALL sp_insert_bitacora(".$idActividad.",".$idUsuario.",".$progreso.",".$this->db->escape($comentario).",1,1)";
+		//echo "QUERY 2: " .$sql;
 		$query = $this->db->query($sql);
 		
-		if (!$query) {
+		$statements = new actividadModel();
+		$data_array = explode("|",$user_rows);
+		$insert_statements = $statements->execUAInsert($data_array, $idUsuario);
+		foreach ($insert_statements as $sql) {
+			$this->db->query($sql);
+		}
+		
+		if($this->db->trans_status() == FALSE) {
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
-	    }
+			$this->db->trans_rollback();
+		} else {
+			$this->db->trans_commit();
+		}
 		
 		return $retArray;
+	}
+	
+	function execUAInsert($data_array, $idUsuarioAsigna){
+		$idActividad = $this->input->post("idActividad");
+		$idUsuarioInsert;
+		$indexTrippin = 0;
+		$trippin;
+
+		foreach ($data_array as $value) {
+			$idUsuarioInsert = $value;
+			if($value == null){
+				return $trippin;
+			}
+			$trippin[$indexTrippin++] = "CALL sp_insert_ua(".$idUsuarioInsert.",".$idActividad.",".$idUsuarioAsigna.")";
+		}
+
+		return  $trippin;
 	}
 	
 	function saveValidation(){
@@ -74,6 +129,140 @@ class actividadModel extends CI_Model{
 		}
 		
 		return $retArray;
+	}
+	
+	function readUsuarios($idActividad){
+		$this->load->database();
+		
+		$page = $this->input->post("page");
+		$limit = $this->input->post("rows");
+		$sidx = $this->input->post("sidx");
+		$sord = $this->input->post("sord");
+		$count = 0;
+		if(!$sidx) $sidx =1;
+		
+		$sql = "SELECT COUNT(*) AS count  FROM Rol r INNER JOIN Rol_Usuario rxu ON r.idRol = rxu.idRol INNER JOIN Usuario u
+    				ON rxu.idUsuario = u.idUsuario
+    				WHERE r.idRol = 1 OR r.idRol = 2 OR r.idRol = 3 OR r.idRol = 4 OR r.idRol = 5";
+		
+		$query = $this->db->query($sql);
+
+		if ($query->num_rows() > 0){
+			$row = $query->row();
+			$count  = $row->count;
+		}
+
+		if( $count >0 ){
+			$total_pages = ceil($count/$limit);
+		}
+		else{
+			$total_pages = 0;
+		}
+
+		if ($page > $total_pages) $page=$total_pages;
+		$start = $limit*$page - $limit;
+
+		$response->page = $page;
+		$response->total = $total_pages;
+		$response->records = $count;
+		
+		//-------------------------------------------------------------------
+		
+		
+		$retArray = array("status" => 0, "msg" => "");
+		
+		$sql = "SELECT u.idUsuario, u.codEmp, CONCAT(u.primerNombre,' ', u.primerApellido,' ') nombre, r.nombreRol
+				FROM Rol r INNER JOIN Rol_Usuario rxu ON r.idRol = rxu.idRol INNER JOIN Usuario u
+        			ON rxu.idUsuario = u.idUsuario
+        		WHERE (r.idRol = 1 OR r.idRol = 2 OR r.idRol = 3 OR r.idRol = 4 OR r.idRol = 5)
+            		AND u.idUsuario NOT IN (SELECT u.idUsuario
+                		FROM Rol r INNER JOIN Rol_Usuario rxu ON r.idRol = rxu.idRol INNER JOIN Usuario u
+	                    	ON rxu.idUsuario = u.idUsuario INNER JOIN Usuario_Actividad uxa
+                    		ON u.idUsuario = uxa.idUsuario
+                		WHERE uxa.idActividad = " . $idActividad . ")";
+		
+		
+		$query = $this->db->query($sql);
+
+		$i = 0;
+		if($query){
+			if($query->num_rows > 0){
+				foreach ($query->result() as $row){
+					$response->rows[$i]["idUsuario"] = $row->idUsuario;
+					$response->rows[$i]["codEmp"] = $row->codEmp;
+					$response->rows[$i]["nombre"] = $row->nombre;
+					$response->rows[$i]["nombreRol"] = $row->nombreRol;
+					$response->rows[$i]["cell"] = array($row->idUsuario, $row->codEmp, $row->nombre, $row->nombreRol);
+					$i++;
+				}
+			}
+		}
+
+		return $response;
+		
+	}
+	
+	function gridUsuarioSet($idUsuario=null){
+		$this->load->database();
+		
+		$page = $this->input->post("page");
+		$limit = $this->input->post("rows");
+		$sidx = $this->input->post("sidx");
+		$sord = $this->input->post("sord");
+		$count = 0;
+		if(!$sidx) $sidx =1;
+		
+		$sql = "SELECT COUNT(*) AS count  FROM Rol r INNER JOIN Rol_Usuario rxu ON r.idRol = rxu.idRol INNER JOIN Usuario u
+    				ON rxu.idUsuario = u.idUsuario
+    				WHERE r.idRol = 1 OR r.idRol = 2 OR r.idRol = 3 OR r.idRol = 4 OR r.idRol = 5";
+		
+		$query = $this->db->query($sql);
+
+		if ($query->num_rows() > 0){
+			$row = $query->row();
+			$count  = $row->count;
+		}
+
+		if( $count >0 ){
+			$total_pages = ceil($count/$limit);
+		}
+		else{
+			$total_pages = 0;
+		}
+
+		if ($page > $total_pages) $page=$total_pages;
+		$start = $limit*$page - $limit;
+
+		$response->page = $page;
+		$response->total = $total_pages;
+		$response->records = $count;
+		
+		//------------------------------------------------------------
+		
+		$sql = "SELECT DISTINCT u.idUsuario, u.codEmp, CONCAT(u.primerNombre,' ', u.primerApellido,' ') nombre, r.nombreRol
+    			FROM Rol r INNER JOIN Rol_Usuario rxu ON r.idRol = rxu.idRol INNER JOIN Usuario u
+    			ON rxu.idUsuario = u.idUsuario INNER JOIN Usuario_Actividad uxa
+    			ON u.idUsuario = uxa.idUsuario
+    			WHERE uxa.idActividad = 1";
+		
+		$query = $this->db->query($sql);
+
+		$i = 0;
+		if($query){
+			if($query->num_rows > 0){
+				foreach ($query->result() as $row){
+					$response->rows[$i]["idUsuario"] = $row->idUsuario;
+					$response->rows[$i]["codEmp"] = $row->codEmp;
+					$response->rows[$i]["nombre"] = $row->nombre;
+					$response->rows[$i]["nombreRol"] = $row->nombreRol;
+					$response->rows[$i]["cell"] = array($row->idUsuario, $row->codEmp, $row->nombre, $row->nombreRol);
+					$i++;
+				}
+			}
+		}
+
+		return $response;
+		
 	}
 	
 }
--- trunk/application/views/actividadView.php
+++ trunk/application/views/actividadView.php
@@ -1,94 +1,127 @@
 <html>
-	<head>
-		<title>Actividades</title>		
+<head>
+<title>Actividades</title>
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/humanity/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
+		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/ui.jqgrid.css" rel="stylesheet" />
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/style.css" rel="stylesheet" />	
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-1.5.2.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-ui-1.8.14.custom.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/grid.locale-es.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery.jqGrid.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/main.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/actividad.js"></script>
-		
-	</head>
-	
-	<body>
-		<div class="menuBar">
-			<ul>
-				<li><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
-				<li class="highlight"><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
-				<li><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
-				<li><span class="menu_button_to"><a href="http://www.google.com"><span class="menu_button_text">Dinamic</span></a></span></li>
-			</ul>			
+
+</head>
+
+<body>
+	<div class="menuBar">
+		<ul>
+			<li><span class="menu_button_to"><a href="http://www.google.com"><span
+						class="menu_button_text">Dinamic</span>
+				</a>
+			</span>
+			</li>
+			<li class="highlight"><span class="menu_button_to"><a
+					href="http://www.google.com"><span class="menu_button_text">Dinamic</span>
+				</a>
+			</span>
+			</li>
+			<li><span class="menu_button_to"><a href="http://www.google.com"><span
+						class="menu_button_text">Dinamic</span>
+				</a>
+			</span>
+			</li>
+			<li><span class="menu_button_to"><a href="http://www.google.com"><span
+						class="menu_button_text">Dinamic</span>
+				</a>
+			</span>
+			</li>
+		</ul>
+	</div>
+
+	<div class="sessionBar">
+		<span id="sessionUser"></span>
+	</div>
+
+	<div>
+		<span id="pageTittle"></span>
+	</div>
+
+	<div class="container" style="height: 870; width: 1100">
+		<div style="height: 20px"></div>
+
+
+		<div id="title" align="center">
+			<font face="calibri" color="green" size="6">Actividad</font>
 		</div>
-		
-		<div class="sessionBar">
-			<span id="sessionUser"></span>
+		<br>
+		<div id="msgBox"></div>
+
+		<div class="divDataForm" style="height: 500">
+			<input id="idEstado" type="hidden" value="" class="hiddenId" /><br> <input
+				id="idActividad" type="hidden" value="1" class="hiddenId" /> <input
+				id="idProyecto" type="hidden" value="1" class="hiddenId" /> <input
+				id="idUsuario" type="hidden" value="1" class="hiddenId" /> <br> <font
+				face="calibri" color="green" size="4">Informacion General</font> <br>
+
+			<span class="inputFieldLabel">Proyecto: </span> <input
+				id="txtProyectoName" type="text" readonly="readonly" value=""
+				class="inputField" /><br> <span class="inputFieldLabel">Actividad: </span>
+			<input id="txtActividadName" type="text" readonly="readonly"
+				class="inputField"></input> <span class="inputFieldLabel">Asignada
+				por: </span> <input id="txtAsignada" type="text" readonly="readonly"
+				class="inputField"></input> <span class="inputFieldLabel">Estado: </span>
+			<select id="cbEstado"></select> <span class="inputFieldLabel">Progreso:
+			</span> <select id="cbProgreso">
+				<option value="">--Progreso--</option>
+				<option value="10">10</option>
+				<option value="20">20</option>
+				<option value="30">30</option>
+				<option value="40">40</option>
+				<option value="50">50</option>
+				<option value="60">60</option>
+				<option value="70">70</option>
+				<option value="80">80</option>
+				<option value="90">90</option>
+				<option value="100">100</option>
+			</select>
+			<!--  <input id="txtProgreso" type="text" class = "inputField"></input> -->
+
+			<span class="inputFieldLabel">Comentarios: </span> <input
+				id="txtComentarios" type="text" class="inputField"></input> <span
+				class="inputFieldLabel">Descripcion: </span>
+			<textArea id="txtDescripcion" readonly="readonly" cols="20" rows="4"
+				class="inputField"></textArea>
+
 		</div>
-		
-		<div><span id="pageTittle"></span></div>
-		
-		<div class="container">
-			<div style="height: 20px"></div>
-			
-			
-			<div id="title" align="center"><font face="calibri" color="green" size="6">Actividad</font></div>
-			<br>	
-			<div id ="msgBox"></div>	
-				
-			<div class="divDataForm" style = "height:500">
-				<input id="idEstado" type="hidden"  value="" class = "hiddenId" /><br>
-				<input id="idActividad" type="hidden" value="1" class = "hiddenId" />
-				<input id="idProyecto" type="hidden" value="1" class = "hiddenId" />
-				<input id="idUsuario" type="hidden" value="1" class = "hiddenId" />
-				<br>
-				<font face="calibri" color="green" size="4">Informacion General</font>
-				<br>
-			
-				<span class = "inputFieldLabel">Proyecto: </span>
-				<input id="txtProyectoName" type="text" readonly="readonly" value="" class = "inputField"/><br>
-				
-				<span class = "inputFieldLabel">Actividad: </span>
-				<input id="txtActividadName" type="text" readonly="readonly" class = "inputField"></input>
-				
-				<span class = "inputFieldLabel">Asignada por: </span>
-				<input id="txtAsignada" type="text" readonly="readonly" class = "inputField"></input>
-				
-				<span class = "inputFieldLabel">Estado: </span>
-				<input id="txtEstadoName" type="text" class = "inputField"></input>
-				
-				<span class = "inputFieldLabel">Progreso: </span>
-				<select id="cbProgreso">
-					<option value="10">10</option>
-					<option value="20">20</option>
-					<option value="30">30</option>
-					<option value="40">40</option>
-					<option value="50">50</option>
-					<option value="60">60</option>
-					<option value="70">70</option>
-					<option value="80">80</option>
-					<option value="90">90</option>
-					<option value="100">100</option>
-				</select>
-				<!--  <input id="txtProgreso" type="text" class = "inputField"></input> -->
-				
-				<span class = "inputFieldLabel">Transferir a: </span>
-				<input id="txtTransferir" type="text" class = "inputField"></input>
-				
-				<span class = "inputFieldLabel">Comentarios: </span>
-				<input id="txtComentarios" type = "text" class = "inputField"></input>
-		
-				<span class = "inputFieldLabel">Descripcion: </span>
-				<textArea id="txtDescripcion" readonly="readonly" cols="20" rows = "4" class = "inputField"></textArea>						
-								
+		<p align="center"><font size="4"><span class="inputFieldLabel">Transferir actividad a usuario: </span></font></p>
+		<table>
+				<tr>
+					<td>
+						<table id="todosUsuarios" align="center"><tr><td/></tr></table>
+						<div id="pager"></div>
+					</td>
+					<td align="center">
+						<button id="btnAsignar" onClick="asignar()">Asignar</button>
+						<button id="btnDesasignar" onClick="desasignar()">Desasignar</button>
+					</td>	
+					<td>
+						<table id="list" align="center"><tr><td/></tr></table>
+						<div id="pagerTR"></div>
+					</td>				
+		</table>
+		<br><br>
+		<div class="divActions">
+			<div class="divCRUDButtons">
+				<button id="btnSave" onClick="save()">Guardar</button>
+				<button id="btnCancel" onClick="cancel()">Cancelar</button>
 			</div>
-			<div class="divActions">				
-				<div class="divCRUDButtons">
-					<button id="btnSave" onClick="save()">Guardar</button>
-					<button id="btnCancel" onClick="cancel()">Cancelar</button>
-				</div>
-			</div>
-			
+
 		</div>
-		
-	</body>
+
+	</div>
+
+</body>
 
 </html>
 
