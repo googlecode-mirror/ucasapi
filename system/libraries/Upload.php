ca4441ce2460ab719e1d8b8e4c09cebb276710fb
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-03 11:41:11 +0000

    modificada línea 920 por un problema con el eval del plugin ajaxupload

--- system/libraries/Upload.php
+++ system/libraries/Upload.php
@@ -917,7 +917,7 @@ class CI_Upload {
 	 * @param	string
 	 * @return	string
 	 */
-	public function display_errors($open = '<p>', $close = '</p>')
+	public function display_errors($open = '', $close = '')//modificado por problemilla con eval, anteriormente: ($open = '<p>', $close = '</p>')
 	{
 		$str = '';
 		foreach ($this->error_msg as $val)
