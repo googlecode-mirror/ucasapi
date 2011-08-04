eda427416f5309fc66541181b5476c411306f681
jlozanoec <jlozanoec@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-04 04:50:49 +0000

    Buzon al 30%

--- application/models/buzonModel.php
+++ application/models/buzonModel.php
@@ -2,19 +2,63 @@
 
 class buzonModel extends CI_Model{
 	
-	function read(){
+	function read($idUsuario){
 		$this->load->database();
+		
+		$page = $this->input->post("page");
+		$limit = $this->input->post("rows");
+		$sidx = $this->input->post("sidx");
+		$sord = $this->input->post("sord");
+		$count = 0;
+		if(!$sidx) $sidx =1;
+		
+		$sql = "SELECT COUNT(*) AS count  FROM Notificacion";
+		
+		$query = $this->db->query($sql);
+
+		if ($query->num_rows() > 0){
+			$row = $query->row();
+			$count  = $row->count;
+		}
+
+		if( $count >0 ){
+			$total_pages = ceil($count/$limit);
+		}
+		else{
+			$total_pages = 0;
+		}
+
+		if ($page > $total_pages) $page=$total_pages;
+		$start = $limit*$page - $limit;
+
+		$response->page = $page;
+		$response->total = $total_pages;
+		$response->records = $count;
+		
+		//-------------------------------------------------------------------
+		
 		$retArray = array("status" => 0, "msg" => "", "data" => array());
 		$idNotificacion = $this->input->post("idNotificacion");
 		
-		$sql = "SELECT notificacion, subject FROM NOTIFICACION WHERE idNotificacion = " .$idNotificacion;
+		$sql = "SELECT n.subject, n.fechaNotificacion, uxn.idEstado
+				FROM Notificacion n INNER JOIN Usuario_Notificacion uxn ON n.idNotificacion = uxn.idNotificacion
+    				INNER JOIN Usuario u ON uxn.idUsuario = u.idUsuario INNER JOIN Estado e ON uxn.idEstado = e.idEstado
+    				INNER JOIN Tipo_Estado te ON e.idTipoEstado = te.idTipoEstado
+				WHERE u.idUsuario = " .$idUsuario.
+				" ORDER BY n.fechaNotificacion";
 		
 		$query = $this->db->query($sql);
 		
-		if($query){
-			$row = $query->row_array();
-			$retArray["data"] = $row;
+		if($query->num_rows > 0){			
+			foreach ($query->result() as $row){		
+				$rowArray = array();
+				$rowArray["subject"] = $row->subject;
+				$rowArray["fechaNotificacion"] = $row->fechaNotificacion;
+				$rowArray["idEstado"] = $row->idEstado;
+				$retArray["data"][] = $rowArray;				
+			}							
 		}
+		
 		else{
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
