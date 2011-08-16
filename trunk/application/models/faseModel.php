6bcb79320e55202061658c67599e3a62bdb6cd6e
nilsinho.1203 <nilsinho.1203@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-16 06:24:15 +0000

    Cambiada la consulta de la fase para obtener la lista "ordenada" de las fases en orden cronologico

--- trunk/application/models/faseModel.php
+++ trunk/application/models/faseModel.php
@@ -1,101 +1,123 @@
 <?php
 
 class faseModel extends CI_Model{
-	
+
 	function autocompleteRead(){
 		$this->load->database();
-		
+
 		$retArray = array("status" => 0, "msg" => "", "data" => array());
-		
-		$sql = "SELECT idFase, nombreFase FROM FASE";
+		$faseList = array();
+		$minFase = 0;
+
+		$sql = "SELECT fa.idFase, fa.nombreFase, fa.idFaseSiguiente, min
+				FROM FASE fa
+				INNER JOIN (select f.idFase min from FASE f
+							where f.idFase NOT IN (select coalesce(a.idFaseSiguiente, -1) from FASE a)) MINFASE
+				ORDER BY idFase";
 		$query = $this->db->query($sql);
-		
+
 		if($query){
 			if($query->num_rows > 0){
-				foreach ($query->result() as $row){
+				/*foreach ($query->result() as $row){
 					$rowArray = array();
 					$rowArray["id"] = $row->idFase;
 					$rowArray["value"] = $row->nombreFase;
 					$retArray["data"][] = $rowArray;
+				}*/
+				foreach ($query->result() as $row){
+					$faseList[$row->idFase] = $row;
+					$minFase = $row->min;
+				}
+
+				$cont = count($faseList);
+
+				while($cont != 0) {
+					$rowArray = array();
+					$rowArray["id"] = $faseList[$minFase]->idFase;
+					$rowArray["value"] = $faseList[$minFase]->nombreFase;
+
+					$retArray["data"][] = $rowArray;
+					$minFase = $faseList[$minFase]->idFaseSiguiente;
+					$cont--;
 				}
 			}
-			
+
 		}
 		else{
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
 		}
-		
+
 		return $retArray;
-		
+
 	}
-	
+
 	function delete(){
 		$this->load->database();
-		
+
 		$retArray = array("status" => "0", "msg" => "");
 		$idFase = $this->input->post("idFase");
-		
+
 		$sql = "DELETE FROM FASE WHERE idFase = " .$idFase;
-		
+
 		$query = $this->db->query($sql);
-		
+
 		if (!$query) {
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
 	    }
-		
+
 		return $retArray;
-		
+
 	}
-	
+
 	function saveValidation(){
 		$this->load->library('form_validation');
-		
+
 		$retArray = array("status"=> 0, "msg" => "");
-		
+
 		$this->form_validation->set_rules("nombreFase", "Nombre", 'required');
-		
+
 		if($this->form_validation->run() == false){
 			$retArray["status"] = 1;
-			
+
 			$retArray["msg"] .= form_error("nombreFase");
 			$retArray["msg"] .= form_error("descripcion");
 		}
-		
+
 		return $retArray;
 	}
-	
+
 	function create(){
 		$this->load->database();
-		
+
 		$retArray = array("status" => 0, "msg" => "");
-		
+
 		$nombreFase = $this->input->post("nombreFase");
 		$descripcion = $this->input->post("descripcion");
-		
+
 		$sql = "INSERT INTO FASE (nombreFase, descripcion) VALUES(" .$this->db->escape($nombreFase). "," .$this->db->escape($descripcion). ")";
-		
+
 		$query = $this->db->query($sql);
-		
+
 		if(!$query){
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
-		}		
-		
+		}
+
 		return $retArray;
 	}
-	
+
 	function read(){
 		$this->load->database();
-		
+
 		$retArray = array("status" => 0, "msg" => "", "data" => array());
 		$idFase = $this->input->post("idFase");
-		
+
 		$sql = 	"SELECT nombreFase, descripcion FROM FASE WHERE idFase = " .$idFase;
-		
+
 		$query = $this->db->query($sql);
-		
+
 		if($query){
 			$row = $query->row_array();
 			$retArray["data"] = $row;
@@ -104,31 +126,31 @@ class faseModel extends CI_Model{
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
 		}
-		
+
 		return $retArray;
-		
+
 	}
-	
+
 	function update(){
 		$this->load->database();
-		
+
 		$retArray = array("status"=> 0, "msg" => "");
-		
+
 		$idFase = $this->input->post("idFase");
 		$nombreFase = $this->input->post("nombreFase");
-		$descripcion = $this->input->post("descripcion");		
-		
-		$sql = "UPDATE FASE SET nombreFase = ".$this->db->escape($nombreFase).", descripcion = ".$this->db->escape($descripcion). " WHERE idFase = " .$idFase; 
-		
+		$descripcion = $this->input->post("descripcion");
+
+		$sql = "UPDATE FASE SET nombreFase = ".$this->db->escape($nombreFase).", descripcion = ".$this->db->escape($descripcion). " WHERE idFase = " .$idFase;
+
 		$query = $this->db->query($sql);
-		
+
 		if (!$query) {
 			$retArray["status"] = $this->db->_error_number();
 			$retArray["msg"] = $this->db->_error_message();
 	    }
-		
-		return $retArray;		
+
+		return $retArray;
 	}
-	
-	
+
+
 }
