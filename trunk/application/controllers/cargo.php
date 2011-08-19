c614dfb96d39222c6c0ff95c23f991d1aa2648d5
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-19 05:55:38 +0000

    Agregado login y tooltips

--- trunk/application/controllers/cargo.php
+++ trunk/application/controllers/cargo.php
@@ -2,8 +2,41 @@
 class Cargo extends CI_Controller{
 
 	function index(){
+		$this->load->library('session');
 		$this->load->helper(array('form', 'url'));
-		$this->load->view("cargoView");
+		$this->load->model("roleOptionsModel");	
+		
+		$controllerName = strtolower(get_class($this));
+		
+		$previousPage = $this->session->userdata("currentPage");
+		$previousPage = ($previousPage!="")?$previousPage:"buzon";
+						
+		$idRol = $this->session->userdata("idRol");//Se agrega en $idRol el dato correspondiente de la sesión
+		
+		if($idRol != ""){//Si está en sesión
+			$allowedPage = $this->roleOptionsModel->validatePage($idRol, $controllerName); 
+			
+			if($allowedPage){//Si el usuario según rol tiene permiso para acceder a la página
+				$this->session->set_userdata("previousPage", $previousPage);
+				$this->session->set_userdata("currentPage", $controllerName);
+				
+				$menu = $this->roleOptionsModel->showMenu($idRol);//Se genera el menú
+				$userName = $this->session->userdata("username");//Se obtiene el nombre de usuario de la sesión
+				$roleName = $this->session->userdata("roleName");
+				
+				$this->load->view("cargoView", array("menu"=> $menu, "userName" => $userName, "roleName" => str_replace("%20", " ", $roleName)));//Se agrega el código del menú y el nombre del usuario como variables al view
+				
+			}
+			else{//Si el usuario no tiene permiso para acceder a la página se redirige a la anterior				
+				$previousPage = $this->session->userdata("currentPage");
+				redirect($previousPage,"refresh");
+			}
+						
+		}
+		else{//Si no existe usuario en sesión se redirige al login
+			redirect("login", "refresh");
+		}
+		
 	}
 	
 	function cargoRead(){
