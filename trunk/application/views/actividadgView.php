745d7f996ad047facbdf2c738d82d69999ebfe8a
jlozanoec <jlozanoec@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-21 06:33:01 +0000

    implementado login

--- trunk/application/views/actividadgView.php
+++ trunk/application/views/actividadgView.php
@@ -1,7 +1,7 @@
 <html>
 <head>
-<title>Test</title>
-		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/humanity/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
+<title>PHOBOS - Actividades asignadas</title>
+		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/horus/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/ui.jqgrid.css" rel="stylesheet" />
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/style.css" rel="stylesheet" />
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-1.5.2.min.js"></script>
@@ -18,27 +18,17 @@
 <body>
 	
 	<div class="menuBar">
-		<ul>
-			<li><span class="menu_button_to"><a href="http://www.google.com"><span
-						class="menu_button_text">Dinamic</span> </a> </span>
-			</li>
-			<li class="highlight"><span class="menu_button_to"><a
-					href="http://www.google.com"><span class="menu_button_text">Dinamic</span>
-				</a> </span>
-			</li>
-			<li><span class="menu_button_to"><a href="http://www.google.com"><span
-						class="menu_button_text">Dinamic</span> </a> </span>
-			</li>
-			<li><span class="menu_button_to"><a href="http://www.google.com"><span
-						class="menu_button_text">Dinamic</span> </a> </span>
-			</li>
-		</ul>
-	</div>
-
-	<div class="sessionBar">
-		<span id="sessionUser"></span>
-	</div>
-
+			<ul>
+				<?php echo $menu;?>
+			</ul>			
+		</div>
+		
+		<div class="sessionBar">
+			<img id="systemIcon" src="<?php echo base_url(); ?>application/views/css/img/gears.png" />	
+			<span id="systemName"><b>PHOBOS PLANING</b></span> 	
+			<img id="logoutButton" title="Cerrar sesión" src="<?php echo base_url(); ?>application/views/css/img/logout_button.png" />
+			<span id="sessionUser"><?php echo  utf8_decode($userName."/".$roleName); ?></span> 
+		</div>
 	<div>
 		<span id="pageTittle"></span>
 	</div>
