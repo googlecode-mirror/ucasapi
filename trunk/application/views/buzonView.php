0ee6eeafe3093ec43690c0d7ef74edd7cd7d2a76
jlozanoec <jlozanoec@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-21 05:55:51 +0000

    implementado login

--- trunk/application/views/buzonView.php
+++ trunk/application/views/buzonView.php
@@ -1,7 +1,7 @@
 <html>
 <head>
-<title>Test</title>
-		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/humanity/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
+<title>PHOBOS - Buzón</title>	
+		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/horus/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/ui.jqgrid.css" rel="stylesheet" />
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/style.css" rel="stylesheet" />
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-1.5.2.min.js"></script>
@@ -9,36 +9,26 @@
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-ui-1.8.14.custom.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/grid.locale-es.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery.jqGrid.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery.bt.min.js"></script>	
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/main.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/buzon.js"></script>
 		<style>
 			.mybold td {font-weight : bold !important}
 		</style>
 </head>
-<body>
-	
+<body>	
 	<div class="menuBar">
-		<ul>
-			<li><span class="menu_button_to"><a href="http://www.google.com"><span
-						class="menu_button_text">Dinamic</span> </a> </span>
-			</li>
-			<li class="highlight"><span class="menu_button_to"><a
-					href="http://www.google.com"><span class="menu_button_text">Dinamic</span>
-				</a> </span>
-			</li>
-			<li><span class="menu_button_to"><a href="http://www.google.com"><span
-						class="menu_button_text">Dinamic</span> </a> </span>
-			</li>
-			<li><span class="menu_button_to"><a href="http://www.google.com"><span
-						class="menu_button_text">Dinamic</span> </a> </span>
-			</li>
-		</ul>
-	</div>
-
-	<div class="sessionBar">
-		<span id="sessionUser"></span>
-	</div>
-
+			<ul>
+				<?php echo $menu;?>
+			</ul>			
+		</div>
+		
+		<div class="sessionBar">
+			<img id="systemIcon" src="<?php echo base_url(); ?>application/views/css/img/gears.png" />	
+			<span id="systemName"><b>PHOBOS PLANING</b></span> 	
+			<img id="logoutButton" title="Cerrar sesión" src="<?php echo base_url(); ?>application/views/css/img/logout_button.png" />
+			<span id="sessionUser"><?php echo  utf8_decode($userName."/".$roleName); ?></span> 
+		</div>
 	<div>
 		<span id="pageTittle"></span>
 	</div>
