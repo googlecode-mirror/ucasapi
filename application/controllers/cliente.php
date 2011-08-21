a77dda5aef20ae33726b8423ffb351e738a29161
jlozanoec <jlozanoec@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-21 18:38:47 +0000

    separación de pantallas

--- application/controllers/cliente.php
+++ application/controllers/cliente.php
@@ -15,33 +15,78 @@ class Cliente extends CI_Controller {
 		}
 	}
 
-	function listaSolicitudes() {
+	function listaSolicitudes(){
 		$this->load->library('session');
 		$this->load->helper(array('form', 'url'));
-
-		$idUsuario = $this->session->userdata("idUsuario");//Se agrega en $idRol el dato correspondiente de la sesiï¿½n
-
-		if($idUsuario == ""){//Si el dato no estï¿½ en sesiï¿½n, se redirige a la pï¿½gina de login
+		$this->load->model("roleOptionsModel");	
+		
+		$controllerName = "cliente/listaSolicitudes";
+		$filePath = base_url()."uploads/";
+		
+		$previousPage = $this->session->userdata("currentPage");
+		$previousPage = ($previousPage!="")?$previousPage:"buzon";
+						
+		$idRol = $this->session->userdata("idRol");//Se agrega en $idRol el dato correspondiente de la sesión
+		
+		if($idRol != ""){//Si está en sesión
+			$allowedPage = $this->roleOptionsModel->validatePage($idRol, $controllerName); 
+			
+			if($allowedPage){//Si el usuario según rol tiene permiso para acceder a la página
+				$this->session->set_userdata("previousPage", $previousPage);
+				$this->session->set_userdata("currentPage", $controllerName);
+				
+				$menu = $this->roleOptionsModel->showMenu($idRol);//Se genera el menú
+				$userName = $this->session->userdata("username");//Se obtiene el nombre de usuario de la sesión
+				$roleName = $this->session->userdata("roleName");
+				$idUsuario = $this->session->userdata("idUsuario");
+				
+				$this->load->view("listaSolicitudesView", array("menu"=> $menu, "idUsuario" => $idUsuario,"userName" => $userName, "roleName" => str_replace("%20", " ", $roleName), "filePath" => $filePath));//Se agrega el código del menú y el nombre del usuario como variables al view
+				
+			}
+			else{//Si el usuario no tiene permiso para acceder a la página se redirige a la anterior				
+				redirect($previousPage,"refresh");
+			}						
+		}
+		else{//Si no existe usuario en sesión se redirige al login
 			redirect("login", "refresh");
-		}
-		else{
-			$this->load->view("listaSolicitudesView");
-		}
+		}		
 	}
 
 	function resumenProyectos() {
 		$this->load->library('session');
 		$this->load->helper(array('form', 'url'));
-
-		$idUsuario = $this->session->userdata("idUsuario");//Se agrega en $idRol el dato correspondiente de la sesiï¿½n
-
-		if($idUsuario == ""){//Si el dato no estï¿½ en sesiï¿½n, se redirige a la pï¿½gina de login
+		$this->load->model("roleOptionsModel");	
+		
+		$controllerName = "cliente/resumenProyectos";
+		$filePath = base_url()."uploads/";
+		
+		$previousPage = $this->session->userdata("currentPage");
+		$previousPage = ($previousPage!="")?$previousPage:"buzon";
+						
+		$idRol = $this->session->userdata("idRol");//Se agrega en $idRol el dato correspondiente de la sesión
+		
+		if($idRol != ""){//Si está en sesión
+			$allowedPage = $this->roleOptionsModel->validatePage($idRol, $controllerName); 
+			
+			if($allowedPage){//Si el usuario según rol tiene permiso para acceder a la página
+				$this->session->set_userdata("previousPage", $previousPage);
+				$this->session->set_userdata("currentPage", $controllerName);
+				
+				$menu = $this->roleOptionsModel->showMenu($idRol);//Se genera el menú
+				$userName = $this->session->userdata("username");//Se obtiene el nombre de usuario de la sesión
+				$roleName = $this->session->userdata("roleName");
+				$idUsuario = $this->session->userdata("idUsuario");
+				
+				$this->load->view("resumenProyectosView", array("menu"=> $menu, "idUsuario" => $idUsuario,"userName" => $userName, "roleName" => str_replace("%20", " ", $roleName), "filePath" => $filePath));//Se agrega el código del menú y el nombre del usuario como variables al view
+				
+			}
+			else{//Si el usuario no tiene permiso para acceder a la página se redirige a la anterior				
+				redirect($previousPage,"refresh");
+			}						
+		}
+		else{//Si no existe usuario en sesión se redirige al login
 			redirect("login", "refresh");
-		}
-		else{
-			$data["idUsuario"] = $idUsuario;
-			$this->load->view("resumenProyectosView", $data);
-		}
+		}	
 	}
 
 	function gridProyectosUsuario($idUsuario) {
