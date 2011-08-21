8b50f3e472387251f14d17f060bda4708dcacfdb
jlozanoec <jlozanoec@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-08-21 09:34:32 +0000

    implementado login

--- trunk/application/controllers/actividad.php
+++ trunk/application/controllers/actividad.php
@@ -1,8 +1,38 @@
 <?php
 class Actividad extends CI_Controller{
 	function index(){
+		$this->load->library('session');
 		$this->load->helper(array('form', 'url'));
-		$this->load->view("actividadView");
+		$this->load->model("roleOptionsModel");	
+		
+		$controllerName = "actividadg";
+		
+		$previousPage = $this->session->userdata("currentPage");
+		$previousPage = ($previousPage!="")?$previousPage:"buzon";
+						
+		$idRol = $this->session->userdata("idRol");//Se agrega en $idRol el dato correspondiente de la sesión
+		
+		if($idRol != ""){//Si está en sesión
+			$allowedPage = $this->roleOptionsModel->validatePage($idRol, $controllerName); 
+			
+			if($allowedPage){//Si el usuario según rol tiene permiso para acceder a la página
+				$this->session->set_userdata("previousPage", $previousPage);
+				$this->session->set_userdata("currentPage", $controllerName);
+				
+				$menu = $this->roleOptionsModel->showMenu($idRol);//Se genera el menú
+				$userName = $this->session->userdata("username");//Se obtiene el nombre de usuario de la sesión
+				$roleName = $this->session->userdata("roleName");
+				
+				$this->load->view("actividadView", array("menu"=> $menu, "userName" => $userName, "roleName" => str_replace("%20", " ", $roleName)));//Se agrega el código del menú y el nombre del usuario como variables al view
+				
+			}
+			else{//Si el usuario no tiene permiso para acceder a la página se redirige a la anterior				
+				redirect($previousPage,"refresh");
+			}						
+		}
+		else{//Si no existe usuario en sesión se redirige al login
+			redirect("login", "refresh");
+		}
 	}
 	
 	function actividadRead(){
