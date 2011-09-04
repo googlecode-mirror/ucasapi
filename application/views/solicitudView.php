ce6294d054d4d21d052dd2515ac68198e7a09d22
nilsinho.1203 <nilsinho.1203@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-09-04 22:19:12 +0000

    correccion soliticitud

--- application/views/solicitudView.php
+++ application/views/solicitudView.php
@@ -1,16 +1,16 @@
 <html>
 	<head>
 		<title>PHOBOS - Solicitud</title>
-		 <meta http-equiv="X-UA-Compatible" content="IE=8" >		
+		 <meta http-equiv="X-UA-Compatible" content="IE=8" >
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/horus/jquery-ui-1.8.14.custom.css" rel="stylesheet" />
 		<link type="text/css" href="<?php echo base_url(); ?>application/views/css/style.css" rel="stylesheet" />
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-1.5.2.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery-ui-1.8.14.custom.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery.bt.min.js"></script>
-		
+
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/validaciones.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/libraries/jquery.simplemodal.1.4.1.min.js"></script>
-		
+
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/main.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>application/views/js/solicitud.js"></script>
 
@@ -23,7 +23,7 @@
 				<?php echo $menu;?>
 			</ul>
 		</div>
-		
+
 		<div id="loading" style="display:none; text-align:center">
 			<span style = "color:white">Obteniendo datos ...</span>
 			<div style="clear:both"></div>
@@ -31,24 +31,24 @@
 		</div>
 
 		<div class="sessionBar">
-			<img id="systemIcon" src="<?php echo base_url(); ?>application/views/css/img/gears.png" />	
-			<span id="systemName"><b>PHOBOS PLANING</b></span> 	
-			<img id="logoutButton" title="Cerrar sesión" src="<?php echo base_url(); ?>application/views/css/img/logout_button.png" />
-			<span id="sessionUser"><?php echo  utf8_decode($userName."/".$roleName); ?></span> 
+			<img id="systemIcon" src="<?php echo base_url(); ?>application/views/css/img/gears.png" />
+			<span id="systemName"><b>PHOBOS PLANING</b></span>
+			<img id="logoutButton" title="Cerrar sesiï¿½n" src="<?php echo base_url(); ?>application/views/css/img/logout_button.png" />
+			<span id="sessionUser"><?php echo  utf8_decode($userName."/".$roleName); ?></span>
 		</div>
 
 		<div><span id="pageTittle"></span></div>
-		
-		
+
+
 		<div class="container" style="height: auto">
-		<br/>	
-		
-			 <div class="divActions"> 
+		<br/>
+
+			 <div class="divActions">
 			 	<div class="divCRUDButtons">
 						<button id="btnSave" onClick="crearSolicitud()">Guardar</button>
 						<button id="btnCancel" onClick="cancel()">Cancelar</button>
 				</div>
-			 </div>			
+			 </div>
 			<div id ="msgBox"></div>
 
 			<div class="divDataForm" style="height: 250px">
@@ -72,15 +72,15 @@
 				<div class="divCRUDRecords">
 					<span class="recordsLabel">Nombre: </span>
 					<input id="txtRecords" type="text"  value="" class="inputFiledAC" /><br>
-					<span class="recordsLabel">Agregados: </span>
+					<span class="recordsLabel">Seguidores: </span>
 					<select size="5" multiple="multiple" id="cbxInteresados" class="selectList"></select><br>
 				</div>
 
 				<div class="divCRUDButtons">
 				<br>
-					<button id="btnDelete" onClick="remove()">Quitar interesado</button>
+					<button id="btnDelete" onClick="remove()">Quitar Seguidor</button>
 				</div>
-				
+
 			</div>
 			<div style="height: 20px"></div>
 
