1f6fa18b1eb8bd3b5fde4f3df05682b585aaa086
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-09-05 01:21:21 +0000

    Se completó el manejo de errores de base de datos y su conexión

--- system/helpers/global_helper.php
+++ system/helpers/global_helper.php
@@ -1,10 +1,19 @@
 <?php
-if ( ! function_exists('database_eror_msg'))
+if ( ! function_exists('database_error_msg'))
 {
-	function database_eror_msg()
+	function database_error_msg()
 	{
 		$CI =& get_instance();
-		return $CI->config->item('database_eror_msg');
+		return $CI->config->item('database_error_msg');
+	}
+}
+
+if ( ! function_exists('database_cn_error_msg'))
+{
+	function database_cn_error_msg()
+	{
+		$CI =& get_instance();
+		return $CI->config->item('database_cn_error_msg');
 	}
 }
 ?>
