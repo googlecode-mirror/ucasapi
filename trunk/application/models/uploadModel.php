a07cd6a081b25a96b7cb5ba4235260748bd53864
beltran.gerson <beltran.gerson@e9f667b9-6ffa-1d13-20fd-08869ab1a34a>
2011-09-05 00:52:49 +0000

    Se completó el manejo de errores de base de datos y su conexión

--- trunk/application/models/uploadModel.php
+++ trunk/application/models/uploadModel.php
@@ -3,9 +3,15 @@ class uploadModel extends CI_Model{
 	
 	//Para completar el nombre que tendrá el  archivo a subir
 	function fileNameCorrelative($uploadIdName, $uploadIdValue){
-		$this->load->database();
-
-		$retArray = array("status"=> 0, "msg" => "", data => "");
+		$retArray = array("status"=> 0, "msg" => "", "data"=>array());
+		
+		$this->load->database();		
+		//Verificando correcta conexión a la base de datos
+		if (!$this->db->conn_id) {
+			$retArray["status"] = 2;
+			$retArray["msg"] = database_cn_error_msg();
+			return $retArray;
+		}
 		
 		$sql = "SELECT COUNT(idArchivo) AS countId FROM ARCHIVOS WHERE $uploadIdName = ".$this->db->escape($uploadIdValue);
 		$query = $this->db->query($sql);
@@ -13,7 +19,7 @@ class uploadModel extends CI_Model{
 		
 		if (!$query){
 	     	$retArray["status"] = $this->db->_error_number();
-			$retArray["msg"] = $this->db->_error_message();
+			$retArray["msg"] = (database_error_msg()!="")?database_error_msg():$this->db->_error_message();
 			return $retArray;
 	    }	
 		
